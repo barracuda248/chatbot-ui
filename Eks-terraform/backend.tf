548cb7d975e2e8ecdd66ea37f1d8163e96ea4834
barracuda248 <163162488+barracuda248@users.noreply.github.com>
2024-03-13 19:52:47 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "openbucket123" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "terraform-state/terraform.tfstate"
     region = "ap-south-1"
+    encrypt=true
   }
 }
